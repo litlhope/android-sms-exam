a89581f82dfee9f93c93ec4641f3f3f614aef84e
Seong-il We <litlhope@gmail.com>
2015-04-30 11:19:53 +0900

    1. 리시버 파일명 수정. 2. SMS 수신시 결과처리할 핸들러를 파라미터로 받도록 수정 3. 핸들러에서 사용할 상수 정

---
+++ app/src/main/java/kr/heja/exam/sms/Const.java
@@ -0,0 +1,12 @@
+package kr.heja.exam.sms;
+
+/**
+ * Created by litlhope on 15. 4. 30..
+ */
+public class Const {
+	public static final int WHAT_SMS_HANDLER = 101;
+
+	public static final String KEY_SMS_DATE = "date";
+	public static final String KEY_SMS_SENDER = "sender";
+	public static final String KEY_SMS_MESSAGE = "message";
+}
